42e6c128161b17e320dd6e79b44831d9bb5abfa5
Nicholas J <nicholas.a.johns5@gmail.com>
2013-10-06 19:11:42 +0000

    I am a tired man.

--- buildr.py
+++ buildr.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
 
+from redis import Redis
 from docker import Client
 from celery import Celery
 from tasks import build
@@ -11,6 +12,8 @@ app = Flask(__name__)
 app.config.from_object('flask_config')
 app.config.from_envvar('BUILDR_CONFIG', silent=True)
 
+redis = Redis(host='localhost', port=6379, db=0)
+
 dc = Client(base_url='unix://var/run/docker.sock', version='1.5')
 
 @app.route('/')
@@ -18,15 +21,16 @@ def show_index():
     images = dc.containers()
     return render_template('index.html', images=images)
 
-@app.route('/payload', methods=['POST'])
+@app.route('/payload', methods=['GET', 'POST'])
 def start_build():
-    payload = json.loads(request.form['payload'])
-    if not payload:
-        abort(400)
-    elif payload['repository']['name'] is not app.config['RS_REPO']:
-        abort(401)
+    # payload = json.loads(request.form['payload'])
+    # if not payload:
+    #     abort(400)
+    # elif payload['repository']['name'] is not app.config['RS_REPO']:
+    #     abort(401)
 
-    res = build()
+    res = build(app.config['RS_REPO_URL'], app.config['RS_TAG'])
+    redis.set('job', res)
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
--- flask_config.py
+++ flask_config.py
@@ -1,3 +1,3 @@
 RS_REPO = 'docker-apache2'
-RS_PATH = 'github.com/taon809/docker-apache2'
+RS_REPO_URL = 'github.com/taon809/docker-apache2'
 RS_TAG = 'johnsn/rs-ap5'
